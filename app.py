d100b366f8f42c393a10c0ebe63d1c02e40dc58d
Watson_J <skfkeh0512@naver.com>
2022-11-29 10:17:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -465,3 +465,11 @@ elif options == '04. 우수 모델 선정':
 
 elif options == '05. Error!!':
     st.title('이게 참 어려웠지~(아련)')
+    st.subheader('3. 각 알고리즘 합치기')
+    
+    st.write('각 알고리즘의 key 값이 충돌해서 plotly 그래프가 그려지지 않는 문제 발생')
+    st.image('https://github.com/skfkeh/MachineLearning/blob/main/img/merge%20algorithm_key%20error.png?raw=true', caption="streamlit error 화면")
+    
+    
+    
+    
